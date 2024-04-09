47fc48e3e74d9996d6346010be8c6332408948aa
Web-Jose <119899203+Web-Jose@users.noreply.github.com>
2024-04-09 06:41:29 -0700

    Functionality

--- scripts.js
+++ scripts.js
@@ -1,94 +1,224 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+// Variables
+var pokemonTeams = [];
+const teamcontainer = document.getElementsByClassName("teams-container")[0];
+var currentTeamIndex = null;
+const searchInput = document.getElementById("pokemon-search");
+let debounceTimeout;
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
-
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+// Utility Functions
+function searchPokemon(searchTerm) {
+  if (searchTerm === "") {
+    document.getElementById("search-results").innerHTML = "";
+  } else {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species?limit=1025`)
+      .then((response) => response.json())
+      .then((data) => {
+        let results = data.results;
+        let filteredResults = results.filter((result) =>
+          result.name.includes(searchTerm.toLowerCase())
+        );
+        renderSearchResults(filteredResults);
+      });
+  }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
-
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+function renderSearchResults(results) {
+  let searchResults = document.getElementById("search-results");
+  searchResults.innerHTML = "";
+  results.forEach((result) => {
+    let pokemonResult = document.createElement("div");
+    pokemonResult.classList.add("pokemon-result");
+    fetch(result.url.replace("-species", ""))
+      .then((response) => response.json())
+      .then((data) => {
+        let pokemon = data;
+        let pokemonSprite = pokemon.sprites.front_default;
+        let pokemonName = pokemon.name;
+        let pokemonTypes = pokemon.types.map((type) => type.type.name);
+        pokemonResult.innerHTML = `
+            <img src="${pokemonSprite}" alt="${pokemonName}" />
+            <span>${
+              pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
+            }</span>
+            <div class="types">
+            ${pokemonTypes
+              .map((type) => `<span class="type ${type}">${type}</span>`)
+              .join("")}
+            </div>
+            `;
+      });
+    pokemonResult.addEventListener("click", () => {
+      if (
+        currentTeamIndex !== null &&
+        pokemonTeams[currentTeamIndex].pokemon.length < 6
+      ) {
+        const pokemon = {
+          name: result.name,
+          url: result.url,
+        };
+        addPokemon(pokemon, currentTeamIndex);
+        currTeamPKMN(currentTeamIndex);
+      } else {
+        alert(
+          "Please select a team or remove a Pokemon from the current team."
+        );
+      }
+    });
+    searchResults.appendChild(pokemonResult);
+  });
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+function renderTeams() {
+  teamcontainer.innerHTML = "";
+  pokemonTeams.forEach((team, teamIndex) => {
+    let teamElement = document.createElement("div");
+    teamElement.classList.add("team");
+    teamElement.innerHTML = `
+        <h3>${team.name}</h3>
+        <div class="team-pokemon"></div>
+        <div class="team-actions">
+            <button onclick="addPokemonButton(${teamIndex})">Add Pokemon</button>
+            <button onclick="removeTeam(${teamIndex})">Delete Team</button>
+        </div>
+        `;
+    let teamPokemon = teamElement.getElementsByClassName("team-pokemon")[0];
+    team.pokemon.forEach((pokemon, pokemonIndex) => {
+      let pokemonElement = document.createElement("div");
+      pokemonElement.classList.add("pokemon");
+      fetch(pokemon.url.replace("-species", ""))
+        .then((response) => response.json())
+        .then((data) => {
+          let pokemonSprite = data.sprites.front_default;
+          let pokemonName = data.name;
+          let pokemonTypes = data.types.map((type) => type.type.name);
+          pokemonElement.innerHTML = `
+                <img src="${pokemonSprite}" alt="${pokemonName}" />
+                <span>${
+                  pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
+                }</span>
+                <div class="types">
+                ${pokemonTypes
+                  .map((type) => `<span class="type ${type}">${type}</span>`)
+                  .join("")}
+                </div>
+                `;
+          pokemonElement.addEventListener("click", () => {
+            removePokemon(pokemonIndex, teamIndex);
+          });
+        });
+      teamPokemon.appendChild(pokemonElement);
+    });
+    teamcontainer.appendChild(teamElement);
+  });
 }
 
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+function currTeamPKMN(teamIndex) {
+  let formTeam = document.getElementsByClassName("pokemon-team")[0];
+  formTeam.innerHTML = "";
+  let team = pokemonTeams[teamIndex];
+  let currentTeam = document.createElement("div");
+  currentTeam.classList.add("current-team");
+  team.pokemon.forEach((pokemon, pokemonIndex) => {
+    let pokemonElement = document.createElement("div");
+    pokemonElement.classList.add("pokemon");
+    fetch(pokemon.url.replace("-species", ""))
+      .then((response) => response.json())
+      .then((data) => {
+        let pokemonSprite = data.sprites.front_default;
+        let pokemonName = data.name;
+        let pokemonTypes = data.types.map((type) => type.type.name);
+        pokemonElement.innerHTML = `
+                    <img src="${pokemonSprite}" alt="${pokemonName}" />
+                    <span>${
+                      pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
+                    }</span>
+                    <div class="types">
+                    ${pokemonTypes
+                      .map(
+                        (type) => `<span class="type ${type}">${type}</span>`
+                      )
+                      .join("")}
+                    </div>
+                    `;
+        pokemonElement.addEventListener("click", () => {
+          removePokemon(pokemonIndex, teamIndex);
+        });
+      });
+    currentTeam.appendChild(pokemonElement);
+  });
+  formTeam.appendChild(currentTeam);
+}
+
+// Event Listeners
+searchInput.addEventListener("keyup", (e) => {
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(() => {
+    searchPokemon(e.target.value);
+  }, 300);
+});
+
+// DOM Manipulation Functions
+function createTeamButton() {
+  document.getElementsByClassName("team-creation-form")[0].style.display =
+    "block";
+  document.getElementsByClassName("tcf-part-1")[0].style.display = "block";
+}
+
+function closeTeamCreation() {
+  document.getElementsByClassName("team-creation-form")[0].style.display =
+    "none";
+}
+
+// Team Management Functions
+function addPokemon(pokemon, teamIndex) {
+  pokemonTeams[teamIndex].pokemon.push(pokemon);
+  renderTeams();
+  currTeamPKMN(teamIndex);
+}
+
+function removePokemon(pokemonIndex, teamIndex) {
+  pokemonTeams[teamIndex].pokemon.splice(pokemonIndex, 1);
+  renderTeams();
+  currTeamPKMN(teamIndex);
+}
+
+function createTeam() {
+  let teamName = document.getElementById("team-name").value;
+  if (teamName) {
+    let team = {
+      name: teamName,
+      pokemon: [],
+    };
+    pokemonTeams.push(team);
+    currentTeamIndex = pokemonTeams.length - 1;
+    document.getElementsByClassName("tcf-part-1")[0].style.display = "none";
+    document.getElementById("team-name").value = "";
+    currTeamPKMN(currentTeamIndex);
+    document.getElementById("pokemon-search").value = "";
+    document.getElementsByClassName("tcf-part-2")[0].style.display = "block";
+  } else {
+    alert("Please enter a team name.");
+  }
+}
+
+function finishTeam() {
+  document.getElementsByClassName("tcf-part-2")[0].style.display = "none";
+  document.getElementsByClassName("team-creation-form")[0].style.display =
+    "none";
+  document.getElementById("pokemon-search").value = "";
+  searchPokemon("");
+  renderTeams();
+}
+
+function addPokemonButton(teamIndex) {
+  currentTeamIndex = teamIndex;
+  document.getElementsByClassName("team-creation-form")[0].style.display =
+    "block";
+  document.getElementsByClassName("tcf-part-1")[0].style.display = "none";
+  document.getElementsByClassName("tcf-part-2")[0].style.display = "block";
+}
+
+function removeTeam(teamIndex) {
+  pokemonTeams.splice(teamIndex, 1);
+  renderTeams();
 }
